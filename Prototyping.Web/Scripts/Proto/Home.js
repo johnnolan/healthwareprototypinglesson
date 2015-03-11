5e80b6cf4825bca8ae9c10c6684e6d7d788f8eb0
JB <john.nolan@north-51.com>
2015-03-11 12:15:28 +0000

    Show hide and toggle proto added

--- Prototyping.Web/Scripts/Proto/Home.js
+++ Prototyping.Web/Scripts/Proto/Home.js
@@ -1,4 +1,21 @@
 ﻿window.onload = function () {
     var validation = new prototypeNamespace.Validation('validate-form');
-    validation.addremove();
+    validation.validate();
+    
+    var manipulation = new prototypeNamespace.DomManipulation();
+    manipulation.addremove(document.getElementsByClassName('validate-form'));
+
+    function showEvent() {
+        manipulation.show(document.getElementById('exampleInputFullName').parentNode);
+    }
+    function hideEvent() {
+        manipulation.hide(document.getElementById('exampleInputFullName').parentNode);
+    }
+    function toggleEvent() {
+        manipulation.toggle(document.getElementById('exampleInputPassword1').parentNode);
+    }
+    
+    document.getElementById('showElement').addEventListener('click', showEvent);
+    document.getElementById('hideElement').addEventListener('click', hideEvent);
+    document.getElementById('toggleElement').addEventListener('click', toggleEvent);
 };
--- Prototyping.Web/Scripts/Proto/ProtoTyping.js
+++ Prototyping.Web/Scripts/Proto/ProtoTyping.js
@@ -1,28 +1,61 @@
 ﻿var prototypeNamespace = prototypeNamespace || {};
 
+prototypeNamespace.DomManipulation = function () {
+};
+
+prototypeNamespace.DomManipulation.prototype = function () {
+    // private members
+
+        // Add/Remove element (http://jsperf.com/basic-prototyping-form-validation)
+    var addremove = function(eqCtl) {
+        for (this.i = 0; this.i < eqCtl.length; this.i++) {
+            var newdiv = document.createElement('div');
+            var divIdName = 'my' + this.i + 'Div';
+            newdiv.setAttribute('id', divIdName);
+            newdiv.innerHTML = 'Element Number ' + this.i + ' has been added! "' + divIdName;
+
+            eqCtl[this.i].appendChild(newdiv);
+
+            var divToRemove = document.getElementById(divIdName);
+            var d = divToRemove.parentNode;
+            d.removeChild(divToRemove);
+        }
+    },
+        show = function(ele) {
+            ele.style.display = "block";
+        },
+        hide = function(ele) {
+            ele.style.display = "none";
+        },
+        toggle = function (ele) {
+            if (ele.style.display !== "block" && ele.style.display !== "none") {
+                ele.style.display = "none";
+            } else if (ele.style.display === "block") {
+                ele.style.display = "none";
+            } else {
+                ele.style.display = "block";
+            }
+        };
+    
+
+    // public members
+    return {
+        addremove: addremove,
+        show: show,
+        hide: hide,
+        toggle: toggle
+    };
+}();
+
 prototypeNamespace.Validation = function (eq) {
     this.eqCtl = document.getElementsByClassName(eq);
 };
 
 prototypeNamespace.Validation.prototype = function () {
     // private members
+    
+    // Basic validation based on data attributes in form
     var validate = function () {
-        for (this.i = 0; this.i < this.eqCtl.length; this.i++) {
-            var newdiv = document.createElement('div');
-            var divIdName = 'my' + this.i + 'Div';
-            newdiv.setAttribute('id', divIdName);
-            newdiv.innerHTML = 'Element Number ' + this.i + ' has been added! "' + divIdName;
-
-            this.eqCtl[this.i].appendChild(newdiv);
-
-            var divToRemove = document.getElementById(divIdName);
-            var d = divToRemove.parentNode;
-            d.removeChild(divToRemove);
-        }
-    };
-
-    // Add/Remove element (http://jsperf.com/basic-prototyping-form-validation)
-    var addremove = function () {
         for (this.i = 0; this.i < this.eqCtl.length; this.i++) {
             var newdiv = document.createElement('div');
             var divIdName = 'my' + this.i + 'Div';
@@ -39,7 +72,6 @@ prototypeNamespace.Validation.prototype = function () {
 
     // public members
     return {
-        validate: validate,
-        addremove: addremove
+        validate: validate
     };
 }();
